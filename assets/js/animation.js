44e491ff26f5baddc0589421431bca213b1f9254
Karen Gomes <karenecris12015@gmail.com>
2023-11-21 15:04:14 -0300

    animation

---
+++ assets/js/animation.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.texto-inicio', { 
+    rotate: {x: 0, y: 40, z: 0},
+    duration: 2000
+});
+sr.reveal('.inicio-botao', {duration: 3000});
+sr.reveal('#perfil-inicio', {duration: 3000});
+
+sr.reveal('.titulo-decorado', {
+    rotate: {x: 0, y: 40, z: 0},    
+    duration: 3000
+});
+
+
+sr.reveal('.texto-sobre-mim h1 ', {duration: 1000});
+sr.reveal('.texto-sobre-mim p ', {duration: 2000});
+sr.reveal('.conteudo img ', {duration: 2000});
+
+sr.reveal('.projeto ', {duration: 2000});
+
